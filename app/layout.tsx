a4600fc30244f09e796cd2036b360fc540e9f0b7
Fredeau123 <frederictrivett@gmail.com>
2024-09-20 17:23:46 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function RootLayout({
